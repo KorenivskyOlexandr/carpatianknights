0e250fbe6db1a18c3ef66070e08e56e79bfa7a34
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2020-06-09 18:17:32 +0300

    test autopull

--- carpatianknights/front_end/views.py
+++ carpatianknights/front_end/views.py
@@ -16,7 +16,7 @@ def main_page(request):
 
 @csrf_exempt
 def update(request):
-    if request.method == "GET":
+    if request.method == "POST":
 
         rc = subprocess.call("prod_git_pull_fetch.sh")
 
