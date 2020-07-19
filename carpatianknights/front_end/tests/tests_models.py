98f3885c99ae29cb12e1f933dfbbee9099f75e48
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2020-07-19 12:53:45 +0300

    add test front end models

---
+++ carpatianknights/front_end/tests/tests_models.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from carpatianknights.front_end.models import Photo
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.test_image = SimpleUploadedFile(name='test_image.jpg',
+                                             content=open(
+                                                 settings.STATIC_ROOT + 'images/test_image.jpg', 'rb').read(),
+                                             content_type='image/jpeg')
+        self.photo1 = Photo.objects.create(image=self.test_image, title='test1')
+
+    def test_creation_photo(self):
+        self.assertEquals(Photo.objects.count(), 1)
+
+    def test_photo_str(self):
+        self.assertEquals(str(self.photo1), 'test1')
+
+    def test_photo_image_resize(self):
+        self.assertGreater(self.test_image.size, self.photo1.image.size)
