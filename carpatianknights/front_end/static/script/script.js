2a2147a0cb998aa72ecc184507a3c5b95738320b
EugenBatenko <y.batenko@codexysoft.com>
2020-08-31 13:17:48 +0300

    Add some changes

--- carpatianknights/front_end/static/script/script.js
+++ carpatianknights/front_end/static/script/script.js
@@ -1,9 +1,31 @@
 // Plugins
-import 'bootstrap/dist/js/bootstrap.bundle'
-
 import {hamburgerToggle} from "./custom/toggle-menu";
+import { CountUp } from 'countup.js/dist/countUp';
 import "bootstrap/dist/js/bootstrap.bundle";
+import "waypoints/lib/noframework.waypoints"
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
     hamburgerToggle();
+    let counter = new CountUp("counter1", 0, 15000, 0, 5, {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.'
+    });
+    counter.start()
 });
+
+
+// const waypoint = new Waypoint({
+//     element: document.getElementById('waypoint1'),
+//     handler: function (direction) {
+//
+//         if (direction === "up") {
+//             counter.reset();
+//         } else {
+//             counter.start();
+//         }
+//
+//     },
+//     offset: '50%'
+// });
