4ded0e2ebbe12b598820962b239931a11357126b
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2020-11-05 11:30:19 +0200

    refactor front_end app

--- carpatianknights/front_end/services.py
+++ carpatianknights/front_end/services.py
@@ -2,17 +2,23 @@ from PIL import Image
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
+from .models import Photo
 
 
 def compress_image(image, size=False):
-    image_temproary = Image.open(image)
-    outputIoStream = BytesIO()
+    image_temp = Image.open(image)
+    output_io_stream = BytesIO()
     if size:
-        imageTemproaryResized = image_temproary.resize(size)
+        image_temp_resized = image_temp.resize(size)
     else:
-        imageTemproaryResized = image_temproary.resize((1920, 1080))
-    imageTemproaryResized.save(outputIoStream, format='JPEG', quality=50)
-    outputIoStream.seek(0)
-    uploaded_image = InMemoryUploadedFile(outputIoStream, 'ImageField', "%s.jpg" % image.name.split('.')[0],
-                                          'image/jpeg', sys.getsizeof(outputIoStream), None)
+        image_temp_resized = image_temp.resize((1920, 1080))
+    image_temp_resized.save(output_io_stream, format='JPEG', quality=50)
+    output_io_stream.seek(0)
+    uploaded_image = InMemoryUploadedFile(output_io_stream, 'ImageField', "%s.jpg" % image.name.split('.')[0],
+                                          'image/jpeg', sys.getsizeof(output_io_stream), None)
     return uploaded_image
+
+
+def get_gallery_image_list():
+    return Photo.objects.filter(
+        title__startswith="gallery")  # в слайдер попадають стиснені фотографігі які починаються на "gallery"
--- carpatianknights/front_end/views.py
+++ carpatianknights/front_end/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 import datetime
-from .models import Photo
+from .services import get_gallery_image_list
 
 
 def main_page(request):
     year = datetime.datetime.now()
-    images_list = Photo.objects.filter(
-        title__startswith="gallery")  # в слайдер попадають стиснені фотографігі які починаються на "gallery"
-    return render(request, 'main.html', {"year": year, "images": images_list})
+    return render(request, 'main.html', {"year": year, "images": get_gallery_image_list()})
