a5784752574d15c77f19c86efc5e7b697bd8b20d
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2021-01-31 15:48:37 +0200

    add head title background on pages and small fix

--- src/js/custom/toggle-menu.js
+++ src/js/custom/toggle-menu.js
@@ -1,17 +1,23 @@
 const bodyScrollLock = require('body-scroll-lock');
 export const hamburgerToggle = () => {
-   const hamburger = document.querySelector('.hamburger');
-   const sideMenu = document.querySelector('.side-menu');
-   const mainBlock = document.querySelector('.main');
+    const hamburger = document.querySelector('.hamburger');
+    const sideMenu = document.querySelector('.side-menu');
+    const mainBlock = document.querySelector('.main');
+    const headers_container = document.querySelector('.headers-container');
 
-   hamburger.addEventListener('click', ()=> {
-      hamburger.classList.toggle('open');
-      sideMenu.classList.toggle('open');
-      mainBlock.classList.toggle('main-rotate');
-      document.body.classList.toggle('open-menu');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        sideMenu.classList.toggle('open');
+        mainBlock.classList.toggle('main-rotate');
+        document.body.classList.toggle('open-menu');
 
-      ((sideMenu.classList.contains("open"))
-        ? bodyScrollLock.disableBodyScroll(sideMenu)
-        : bodyScrollLock.enableBodyScroll(sideMenu))
-   });
+        ((sideMenu.classList.contains("open"))
+            ? bodyScrollLock.disableBodyScroll(sideMenu)
+            : bodyScrollLock.enableBodyScroll(sideMenu))
+        try {
+            headers_container.classList.toggle('close');
+        } catch (e) {
+        }
+
+    });
 }
