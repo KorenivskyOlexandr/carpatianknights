e191041a531a7ef4373bafb3db487c07899e2c46
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2021-02-14 13:36:24 +0200

    create model to upload files

--- carpatianknights/front_end/admin.py
+++ carpatianknights/front_end/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, File
 
 admin.site.register(Photo)
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
--- carpatianknights/front_end/models.py
+++ carpatianknights/front_end/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .services import compress_image
+from django.utils.text import slugify
 
 
 class Photo(models.Model):
@@ -16,3 +17,20 @@ class Photo(models.Model):
         if not self.id:
             self.image = compress_image(self.image)
         super(Photo, self).save(*args, **kwargs)
+
+
+class File(models.Model):
+    file = models.FileField(upload_to='files/')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(File, self).save(*args, **kwargs)
